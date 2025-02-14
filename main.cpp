de7c57f682c94d1c49b94f24536d021f906b5060
SampiNova <dmitry.karpachev@hotmail.com>
2025-02-14 05:14:01 +0000

    main.cpp updated

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 #include <memory>
 
 
-#ifdef USE_FLOAT
-typedef float value;
-#else
+#ifdef USE_DOUBLE
 typedef double value;
+#else
+typedef float value;
 #endif
 
 
@@ -15,11 +15,14 @@ int main() {
     unsigned n{10000000};
     auto sin_row { std::make_unique<value[]>(n) };
 
+    value tmp;
     value summ = 0.0;
     value step = 2 * M_PI / (value)n;
     for (unsigned i = 0; i < n; i++) {
-        sin_row[i] = i * step;
-        summ += i * step;
+        tmp = i * step;
+        tmp = sin(tmp);
+        sin_row[i] = tmp;
+        summ += tmp;
     }
 
     std::cout << summ << "\n";
